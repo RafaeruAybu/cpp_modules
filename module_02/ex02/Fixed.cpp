9957e1b108f6f2ce2ede0d8c1125520865fff80e
Isadora Gearhead <igearhea@pr-h3.kzn.21-school.ru>
2021-11-26 06:29:13 +0300

    ex02todo

--- module_02/ex02/Fixed.cpp
+++ module_02/ex02/Fixed.cpp
@@ -40,10 +40,144 @@ Fixed& Fixed::operator= (const Fixed &p1)
 	return *this;
 }
 
+Fixed	Fixed::operator+ (const Fixed& p1)	const
+{
+	Fixed tmp;
+	tmp.setRawBits(this->getRawBits() + p1.getRawBits());
+	return tmp;
+}
+
+Fixed	Fixed::operator- (const Fixed& p1)	const
+{
+	Fixed tmp;
+	tmp.setRawBits(this->getRawBits() - p1.getRawBits());
+	return tmp;
+}
+
+Fixed	Fixed::operator* (const Fixed& p1)	const
+{
+	Fixed tmp;
+
+	tmp.setRawBits((this->getRawBits() * p1.getRawBits()) >> this->num_of_frctnl_bits);
+	return tmp;
+}
+
+Fixed	Fixed::operator/ (const Fixed& p1)	const
+{
+	Fixed tmp;
+
+	tmp.setRawBits(((this->getRawBits() << this->num_of_frctnl_bits) / p1.getRawBits()));
+	return tmp;
+}
+
+bool	Fixed::operator> (const Fixed& p1) const
+{
+	if (this->getRawBits() > p1.getRawBits())
+		return true;
+	return false;
+}
+
+bool	Fixed::operator< (const Fixed& p1) const
+{
+	if (this->getRawBits() < p1.getRawBits())
+		return true;
+	return false;
+}
+
+bool	Fixed::operator>= (const Fixed& p1) const
+{
+	if (this->getRawBits() >= p1.getRawBits())
+		return true;
+	return false;
+}
+
+bool	Fixed::operator<= (const Fixed& p1) const
+{
+	if (this->getRawBits() <= p1.getRawBits())
+		return true;
+	return false;
+}
+
+bool	Fixed::operator== (const Fixed& p1) const
+{
+	if (this->getRawBits() == p1.getRawBits())
+		return true;
+	return false;
+}
+
+bool	Fixed::operator!= (const Fixed& p1) const
+{
+	if (this->getRawBits() != p1.getRawBits())
+		return true;
+	return false;
+}
+
+//pre
+Fixed&	Fixed::operator++ ()
+{
+	_fixed++;
+	return *this;
+}
+
+//post
+Fixed	Fixed::operator++ (int)
+{
+	Fixed tmp(*this);
+	this->_fixed++;
+	return tmp;
+}
+
+//pre
+Fixed&	Fixed::operator-- ()
+{
+	_fixed--;
+	return *this;
+}
+
+//post
+Fixed	Fixed::operator-- (int)
+{
+	Fixed tmp(*this);
+	this->_fixed--;
+	return *this;
+}
+
 std::ostream& operator<< (std::ostream &out, Fixed const &current)
 {
 	out << current.toFloat();
 	return out;
+}
+
+//min
+Fixed&	Fixed::min(Fixed& p1, Fixed& p2)
+{
+	if (p1.getRawBits() <= p2.getRawBits())
+		return p1;
+	return p2;
+}
+
+//const overload min
+const Fixed& Fixed::min(const Fixed& p1, const Fixed& p2)
+{
+	if (p1.getRawBits() <= p2.getRawBits())
+		return p1;
+	return p2;
+}
+
+//max
+Fixed&	Fixed::max(Fixed& p1, Fixed& p2)
+{
+	if (p1.getRawBits() >= p2.getRawBits())
+		return p1;
+	return p2;
+}
+
+//const overload max
+const Fixed& Fixed::max(const Fixed& p1, const Fixed& p2)
+{
+	if (p1.getRawBits() >= p2.getRawBits())
+		return p1;
+	return p2;
 }
 
 int Fixed::getRawBits(void) const
--- module_02/ex02/Fixed.hpp
+++ module_02/ex02/Fixed.hpp
@@ -1,5 +1,5 @@
-#ifndef MODULE_02_EX01_FIXED_HPP
-#define MODULE_02_EX01_FIXED_HPP
+#ifndef MODULE_02_EX02_FIXED_HPP
+#define MODULE_02_EX02_FIXED_HPP
 #include <iostream>
 #include <cmath>
 
@@ -13,17 +13,34 @@ public:
 
 	~Fixed();
 
-	Fixed& operator= (const Fixed& p1);
+	Fixed&	operator= (const Fixed& p1);
+	Fixed&	operator++ ();
+	Fixed	operator++ (int);
+	Fixed&	operator-- ();
+	Fixed	operator-- (int);
+	Fixed	operator+ (const Fixed& p1)	 const;
+	Fixed	operator- (const Fixed& p1)	 const;
+	Fixed	operator* (const Fixed& p1)	 const;
+	Fixed	operator/ (const Fixed& p1)	 const;
+	bool	operator> (const Fixed& p1) const;
+	bool	operator< (const Fixed& p1) const;
+	bool	operator>= (const Fixed& p1) const;
+	bool	operator<= (const Fixed& p1) const;
+	bool 	operator== (const Fixed& p1) const;
+	bool 	operator!= (const Fixed& p1) const;
 
 	int		getRawBits(void)			const;
-	void	setRawBits(int const raw);
 	float	toFloat(void)				const;
 	int		toInt(void)					const;
+	void	setRawBits(int const raw);
+	static Fixed&	min(Fixed& p1, Fixed& p2);
+	static const Fixed&	min(const Fixed& p1, const Fixed& p2);
+	static Fixed&	max(Fixed& p1, Fixed& p2);
+	static const Fixed&	max(const Fixed& p1, const Fixed& p2);
 private:
 	int _fixed;
 	static const int num_of_frctnl_bits;
 };
 std::ostream& operator<< (std::ostream &out, Fixed const &current);
 
-
-#endif //MODULE_02_EX01_FIXED_HPP
+#endif //MODULE_02_EX02_FIXED_HPP
